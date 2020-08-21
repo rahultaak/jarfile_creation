8b0adffe03a86060ecd4f389c8fa80a270303867
rahultaak <53658875+rahultaak@users.noreply.github.com>
2020-08-21 18:29:57 +0530

    i am uploading code of jar creation

---
+++ src/pkg1/Class1.java
@@ -0,0 +1,10 @@
+package pkg1;
+
+public class Class1 {
+	
+	public void method1()
+	{
+		System.out.println("i am method 1");
+	}
+
+}
---
+++ src/pkg1/Class2.java
@@ -0,0 +1,10 @@
+package pkg1;
+
+public class Class2 {
+	
+	public void method2()
+	{
+		System.out.println("i am method 2");
+	}
+
+}
